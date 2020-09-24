6512f9e4ebd159af52f8f512cabbdd78aeb09a66
José Gabriel Vázquez Gil <josegabrielvazquezgil.saladillo@gmail.com>
2020-09-24 11:11:55 +0200

    Implementation of textViews/id

--- app/src/main/java/es/iessaladilo/jgvg/pr01_what_time_is_it_jgvg/MainActivity.java
+++ app/src/main/java/es/iessaladilo/jgvg/pr01_what_time_is_it_jgvg/MainActivity.java
@@ -3,6 +3,9 @@ package es.iessaladilo.jgvg.pr01_what_time_is_it_jgvg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asigno a variables Ids en JAVA las Ids de mis textViews.
+        TextView lblDate = findViewById(R.id.lbl_date_id);
+        TextView lblTime = findViewById(R.id.lbl_time_id);
+
+        //Creo las fechas y la hora.
+
 
 
     }
