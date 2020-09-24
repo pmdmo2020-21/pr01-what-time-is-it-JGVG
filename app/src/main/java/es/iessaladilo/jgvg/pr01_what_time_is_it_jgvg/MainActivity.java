31909262335a62b8c002813488d74ae5242e1ebb
José Gabriel Vázquez Gil <josegabrielvazquezgil.saladillo@gmail.com>
2020-09-24 10:51:13 +0200

    Creation of proyect

---
+++ app/src/main/java/es/iessaladilo/jgvg/pr01_what_time_is_it_jgvg/MainActivity.java
@@ -0,0 +1,16 @@
+package es.iessaladilo.jgvg.pr01_what_time_is_it_jgvg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
