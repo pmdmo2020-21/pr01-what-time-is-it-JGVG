6cfb34b4e3ab4082fd75976b8bb724cdacb1721d
José Gabriel Vázquez Gil <josegabrielvazquezgil.saladillo@gmail.com>
2020-09-26 12:32:09 +0200

    Proyecto finalizado

--- app/src/main/java/es/iessaladilo/jgvg/pr01_what_time_is_it_jgvg/MainActivity.java
+++ app/src/main/java/es/iessaladilo/jgvg/pr01_what_time_is_it_jgvg/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Asigno a variables Ids en JAVA las Ids de mis textViews.
+        //Asignando variables de TextView via Id.
         TextView lblDate = findViewById(R.id.lbl_date_id);
         TextView lblTime = findViewById(R.id.lbl_time_id);
 
-        //Creo las fechas y la hora.
+        Date date = new Date();
 
+        SimpleDateFormat patternDate = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat patternTime = new SimpleDateFormat("H:mm");
 
+        lblDate.setText(patternDate.format( date.getTime()));
+        lblTime.setText(patternTime.format( date.getTime()));
 
     }
 }
